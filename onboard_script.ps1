7a1e5f3a14fd2b26878a2c531227abf323bb8477
Yusuph Mkangara <yusuph.mka@outlook.com>
2020-03-09 23:36:08 -0700

    Update onboarding script -- ready for test run

--- onboard_script.ps1
+++ onboard_script.ps1
@@ -4,16 +4,25 @@
 # Get Anaconda
 curl -o conda5_2.exe https://repo.anaconda.com/archive/Anaconda3-5.2.0-Windows-x86_64.exe
 .\conda5_2.exe /AddToPath=1 /RegisterPython=1 # Use /? to see more arguments/flags
+conda --version #Check that it works
+conda update -n base -c defaults conda #Update conda
 
 # Build Tools
 # --help to see more arguments/flags
 # Found this by inspecting the XHR after clicking in the home page, just in case it changes in future
 curl -o vsbuild_tools.exe https://download.visualstudio.microsoft.com/download/pr/378e5eb4-c1d7-4c05-8f5f-55678a94e7f4/a022deec9454c36f75dafe780b797988b6111cfc06431eb2e842c1811151c40b/vs_BuildTools.exe
+.\vsbuild_tools.exe --all --passive --norestart --wait
+
+# Create development environment and install packages using pip
+conda create --name summit python=3.6.*
+activate summit
+which python #Confirm Python path and version
+pip install -r summit_requirements.txt #This will take a while
 
 # Selenium Driver
 # Determine if user has already downloaded Chrome and what version of Chrome they have
 # Download compatible version of the Driver/Chrome
 
 
-Learned a bit about flagging for help from the command line for .exe executables
-https://stackoverflow.com/a/11593467
+# Learned a bit about flagging for help from the command line for .exe executables
+# https://stackoverflow.com/a/11593467
