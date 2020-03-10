eb8ecd01aea3f3a4afb8606688db58dc6c0d474f
Yusuph Mkangara <yusuph.mka@outlook.com>
2020-03-10 01:01:41 -0700

    Update setup steps
    More details to initial setup
    Added assume yes to conda lines

--- onboard_script.ps1
+++ onboard_script.ps1
@@ -1,30 +1,43 @@
 # Attempt to see if it's possible to have an onboarding script
-# Download CMDER first
+# Download CMDER first https://cmder.net/
+# Unzip cmder, put the folder wherever it is easy access. Recommend the desktop. Additionally the cmder executable (green icon) item
+# in the folder is recommended to be added as a shortcut either on the desktop, the taskbar, or in the start menu for quick access.
+# Open up cmder, it will pause for a moment for an initial setup
+# Ctrl + T to open a new tab. A prompt pops up; in the bottom right, check off "Run as Administrator" and Start and Yes
+# type powershell and press Enter
+# The prompt should now say: Windows PowerShell ...
+# On File Explorer, find the folder that has the onboarding script and the summit_requirements.txt file
+# type cd and then drag that folder to the cmder screen, press Enter
+# type .\onboard_script.ps1 and press Enter
+# This will take a while but you should see a "Writing web request..." textbox up top and text that says Downloading Anaconda
+# Hopefully the rest takes care of itself.
 
 # Get Anaconda
-echo Downloading Anaconda
+echo "Downloading Anaconda"
 curl -o conda5_2.exe https://repo.anaconda.com/archive/Anaconda3-5.2.0-Windows-x86_64.exe
-.\conda5_2.exe /AddToPath=1 /RegisterPython=1 # Use /? to see more arguments/flags
+.\conda5_2.exe /AddToPath=1 /RegisterPython=1 /S /D=$ENV:UserProfile\Anaconda3 # Use /? to see more arguments/flags
 conda --version #Check that it works
-conda update -n base -c defaults conda #Update conda
-echo Finished updating Conda
+#conda update -n base -c defaults conda -y #Update conda
+#echo "Finished updating Conda"
 
 # Build Tools
 # --help to see more arguments/flags
 # Found this by inspecting the XHR after clicking in the home page, just in case it changes in future
-echo Downloading Build Tools
+echo "Downloading Build Tools"
 curl -o vsbuild_tools.exe https://download.visualstudio.microsoft.com/download/pr/378e5eb4-c1d7-4c05-8f5f-55678a94e7f4/a022deec9454c36f75dafe780b797988b6111cfc06431eb2e842c1811151c40b/vs_BuildTools.exe
 .\vsbuild_tools.exe --all --passive --norestart --wait
-echo Finished installing Build Tools
+echo "Finished installing Build Tools"
 
 # Create development environment and install packages using pip
-echo Creating a Summit Environment
-conda create --name summit python=3.6.*
-activate summit
+echo "Creating a Summit Environment"
+conda create --name summit python=3.6.* -y
+conda init powershell #This might cause trouble, didn't seem to take effect when I tried activating environment
+powershell #Maybe if start a new shell it'll catch on?
+conda activate summit
 which python #Confirm Python path and version
 pip install -r summit_requirements.txt #This will take a while
-echo Finished creating a Summit environment successfully. Run `activate summit` to begin your adventure
-deactivate
+echo "Finished creating a Summit environment successfully. Run `activate summit` to begin your adventure"
+exit
 
 # Selenium Driver
 # Determine if user has already downloaded Chrome and what version of Chrome they have
